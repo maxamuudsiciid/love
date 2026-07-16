cf3f8c8d4d7ca69f1fc471484c8289fe681e5965
maxamuudsiciid <maxamudsiciid045@gmail.com>
2026-07-16 15:15:03 +0300

    Add showMessage function to toggle message box visibility

--- script.js
+++ script.js
@@ -49,3 +49,14 @@ function animate() {
 }
 
 animate();
+function showMessage(){
+
+    const box = document.getElementById("messageBox");
+
+    if(box.style.display==="block"){
+        box.style.display="none";
+    }else{
+        box.style.display="block";
+    }
+
+}
